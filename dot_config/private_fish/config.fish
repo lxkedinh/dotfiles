6269bf68613325eda1a08ba67a2306893eb00958
Luke Dinh <lukexdinh@gmail.com>
2023-09-07 13:50:04 -0700

    added yarn global bin to system path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,6 +6,7 @@ set -U fish_greeting
 
 set -gx PATH $PATH /home/lukedinh/.local/bin
 set -gx PATH $PATH /home/lukedinh/.npm-global
+set -gx PATH $PATH /home/lukedinh/.yarn/bin
 set -gx XDG_SESSION_DESKTOP hyprland
 # tell fish to use LunarVim as default editor
 set -gx EDITOR lvim
